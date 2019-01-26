f2b23ff2a18167eb7dead6a23a406f9c04f78866
Aron <aron.castro.coelho@gmail.com>
2019-01-26 00:57:31 -0200

    Add registry to migrations

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,16 +1,29 @@
+var Wallet = artifacts.require("Wallet");
 var Store = artifacts.require("Store");
 var Token = artifacts.require("Token");
 var Joke = artifacts.require("Joke");
-var Wallet = artifacts.require("Wallet");
+var Registry = artifacts.require("Registry");
 
 module.exports = async (deployer) => {
-  var token, wallet;
+  var registry, token, wallet, store;
+
+  await deployer.deploy(Registry);
+  registry = await Registry.deployed();
 
   await deployer.deploy(Token);
   token = await Token.deployed();
-  await deployer.deploy(Wallet, token.address);
-  wallet = await Wallet.deployed();
+  await registry.register("token", token.address);
 
-  await deployer.deploy(Store, token.address, wallet.address);
+  await deployer.deploy(Wallet, registry.address);
+  wallet = await Wallet.deployed();
+  await registry.register("wallet", wallet.address);
+
+  await deployer.deploy(Store, registry.address);
+  store = await Store.deployed();
+  await registry.register("store", store.address);
+
   await deployer.deploy(Joke);
+
+  await token.transfer(wallet.address, await token.INITIAL_SUPPLY());
+  await wallet.approve(store.address);
 };
