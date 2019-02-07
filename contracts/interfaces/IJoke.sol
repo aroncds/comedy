66847a1270ab4f5b5e99701f071ee41c67f02973
Aron Castro <aron.castro.coelho@gmail.com>
2019-02-06 22:06:22 -0200

    Add get joke by address

--- contracts/interfaces/IJoke.sol
+++ contracts/interfaces/IJoke.sol
@@ -1,11 +1,17 @@
 pragma solidity ^0.5.0;
 
-contract IJoke {
+interface IJoke {
     event Create(address indexed owner, uint256 id);
+    event Like(address indexed from, address indexed to, uint256 units);
 
-    function create(string memory body) public;
-    function get(uint256 i)
-        public
-        view
-        returns(uint256, uint256, uint256, uint256, address, string memory);
+    function create(string calldata body) external;
+    function addLike(uint256 id, uint256 units) external;
+    function length() external view returns(uint256);
+    function joke(uint256 i) external view returns(
+        uint256, uint256, uint256,
+        uint256, address, string memory);
+    function jokeByAddressLength(address owner) external view returns(uint256);
+    function jokeByAddress(address owner, uint256 index) external view returns(
+        uint256, uint256, uint256,
+        uint256, address, string memory);
 }
--- test/TestJoke.js
+++ test/TestJoke.js
@@ -21,17 +21,17 @@ contract('Joke', function(accounts){
 
         instance = await Joke.new(registry.address);
 
-        var amount = await token.balanceOf(accounts[0]);
+        let amount = await token.balanceOf(accounts[0]);
 
         await token.transfer(wallet.address, amount - 500);
         await wallet.approve(instance.address);
     });
 
     it('should write a new joke', async () => {
-        var message = "It's funny";
+        let message = "It's funny";
         
         await instance.create(message);
-        var data = await instance.joke(0);
+        let data = await instance.joke(0);
 
         assert.equal(data[0].toNumber(), 1, "check if version code is 1");
         assert.equal(data[3].toNumber(), 0, "check if likes is initialize with 0");
@@ -40,15 +40,28 @@ contract('Joke', function(accounts){
 
     it('should send one like to joke', async() => {
         await token.approve(wallet.address, 100);
-
         await instance.create("test", {from: accounts[1]});
         await instance.addLike(0, 100);
 
-        var data = await instance.joke(0);
-        var amount = await token.balanceOf(accounts[1]);
+        let data = await instance.joke(0);
+        let amount = await token.balanceOf(accounts[1]);
 
         assert.equal(data[3].toNumber(), 1, "check if like was computate");
         assert.equal(amount.toNumber(), 100, "check if token was removed");
+    });
+
+    it('should get a joke by owner\'s address', async () => {
+        let message = "It's funny";
+
+        await instance.create(message);
+
+        let len = await instance.jokeByAddressLength(accounts[0]);
+        let data = await instance.jokeByAddress(accounts[0], 0);
+
+        assert.equal(len, 1, "check if length is equal to 1");
+        assert.equal(data[0].toNumber(), 1, "check if version code is 1");
+        assert.equal(data[3].toNumber(), 0, "check if likes is initialize with 0");
+        assert.equal(data[5].toString(), message, "check if message storaged is the same");
     });
 
     it('should revert when it inst approved', async() => {
@@ -57,7 +70,7 @@ contract('Joke', function(accounts){
             await instance.create("test", {from: accounts[1]});
             await instance.addLike(0, 100);
 
-            var data = await instance.joke(0);
+            let data = await instance.joke(0);
 
             assert.equal(data[3].toNumber(), 0, "no approved");
         }catch(e){
@@ -70,23 +83,8 @@ contract('Joke', function(accounts){
 
         try{
             await instance.create("aron");
-            assert.equal(1,0, "no paused");
-        }catch(e){}
+        }catch(e){
+            assert.notEqual(e.message.indexOf("revert"), -1, "check if ")
+        }
     });
-
-    /*it('should upgrade the contract', async () => {
-        var instance2 = await Joke.new();
-
-        await instance.setUpgrade(instance2.address, {from: accounts[0]});
-
-        var contractAddress = await web3.eth.getStorageAt(instance.address, 4);
-        //console.log(contractAddress)
-
-        await instance.create("Aron");
-
-        var data = await instance.joke(0);
-
-        assert.equal(contractAddress, instance2.address, "check if contract has been upgraded");
-        assert.equal(data[5], "Aron", "check if data have been storaged");
-    });*/
 });
